cb86169cf7262e4a197ce9360fc296341002dc0c
ledinhdoan <ledinhdoan.0083@gmail.com>
2017-09-21 11:05:18 +0700

    fix bug

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
+  CRLF = "\r\n"
+  LF = "\n"
+
   mount_uploader :image, ImageUploader
   belongs_to :category
   belongs_to :user
-
-  scope :for_boy, ->category_ids{where(category_id: category_ids)}
 end
